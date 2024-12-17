0c66d481fcfa7ec46793d97e1b39f61ab305f1ae
Jolusca <jlucasoliveira2002@gmail.com>
2024-12-17 19:09:20 -0300

    sensor de tensão e vetor mensagem

--- SensorLuminosidadeEsp32/src/main.cpp
+++ SensorLuminosidadeEsp32/src/main.cpp
@@ -17,21 +17,21 @@ const char* password = "SUA_SENHA"; // Insira a senha da rede Wi-Fi
 #define LED_PIN2 26 // Pino digital onde o LED2 está conectado
 #define LED_PIN3 27 // Pino digital onde o LED3 está conectado
 #define LED_PIN4 14 // Pino digital onde o LED4 está conectado
-#define LED_TEMP 13 // Novo LED que será ativado caso a temperatura ultrapasse 50 graus
+//#define LED_TEMP 13 // Novo LED que será ativado caso a temperatura ultrapasse 50 graus
 
 // Definição do pino do sensor NTC (termistor)
 #define NTC_PIN 12 // Pino analógico onde o NTC está conectado
 
+// Pino para leitura da tensão
+#define VOLTAGE_PIN 13
+
+//Definindo resistores para o divisor de tensao
+const float R1 = 34000.0;//33k
+const float R2 = 9800.0;//10k
+
 // Variáveis para controle de tempo
 unsigned long lastReadTime = 0; // Armazena o último momento de leitura dos sensores
 unsigned long readInterval = 100; // Intervalo para leitura dos sensores (em milissegundos)
-
-/*void conectarWiFi() {
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-  }
-}*/
 
 float lerNTC(int pin) {
   int rawValue = analogRead(pin); // Lê o valor analógico do pino
@@ -47,6 +47,37 @@ float lerNTC(int pin) {
   return temperature; // Retorna a temperatura em Celsius
 }
 
+float lerTensao(int pin) {
+  int rawValue = analogRead(pin); // Lê o valor analógico do pino
+  float voltage = rawValue * (3.3 / 4095.0); // Converte o valor lido para tensão
+  float realVoltage = voltage * ((R1 + R2) / R2); // Converte a tensão medida para a tensão real da fonte
+  return realVoltage;
+}
+
+void conectarWiFi() {
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+}
+ void imprimirMensagem(float mensagem[]){
+  Serial.print("LDRs: ");
+  Serial.print(mensagem[0]);
+  Serial.print(" ");
+  Serial.print(mensagem[1]);
+  Serial.print(" ");
+  Serial.print(mensagem[2]);
+  Serial.print(" ");
+  Serial.print(mensagem[3]);
+  Serial.print(" | Temperatura: ");
+  Serial.print(mensagem[4], 2);
+  Serial.print("C | Tensao: ");
+  Serial.print(mensagem[5], 2);
+  Serial.println("V");
+
+  //atraso de 2 seg
+  delay(2000);
+ }
 void setup() {
   // Conecta ao Wi-Fi
   //conectarWiFi();
@@ -56,7 +87,7 @@ void setup() {
   pinMode(LED_PIN2, OUTPUT);
   pinMode(LED_PIN3, OUTPUT);
   pinMode(LED_PIN4, OUTPUT);
-  pinMode(LED_TEMP, OUTPUT); // Configura o pino do LED de temperatura como saída
+  //pinMode(LED_TEMP, OUTPUT); // Configura o pino do LED de temperatura como saída
 
   // Define os pinos dos LDRs como entrada
   pinMode(LDR_PIN1, INPUT);
@@ -66,6 +97,12 @@ void setup() {
 
   // Define o pino do NTC como entrada (opcional, pois analogRead já faz isso automaticamente)
   pinMode(NTC_PIN, INPUT);
+  
+  // Define o pino de tensão como entrada
+  pinMode(VOLTAGE_PIN, INPUT);
+
+  // Inicializa a comunicação serial para depuração
+  Serial.begin(115200);
 }
 
 void loop() {
@@ -78,36 +115,49 @@ void loop() {
     int ldrValue4 = analogRead(LDR_PIN4);
 
     float temperatura = lerNTC(NTC_PIN);
+    float tensao = lerTensao(VOLTAGE_PIN);
 
-    if (ldrValue1 < THRESHOLD) {
+    float mensagem[6];
+
+    mensagem[0] = (ldrValue1 < THRESHOLD) ? 0 : 1;
+    mensagem[1] = (ldrValue2 < THRESHOLD) ? 0 : 1;
+    mensagem[2] = (ldrValue3 < THRESHOLD) ? 0 : 1;
+    mensagem[3] = (ldrValue4 < THRESHOLD) ? 0 : 1;
+    mensagem[4] = temperatura;
+    mensagem[5] = tensao;
+
+    if (mensagem[0] == 0) {
       digitalWrite(LED_PIN1, HIGH);
     } else {
       digitalWrite(LED_PIN1, LOW);
     }
 
-    if (ldrValue2 < THRESHOLD) {
+    if (mensagem[1] == 0) {
       digitalWrite(LED_PIN2, HIGH);
     } else {
       digitalWrite(LED_PIN2, LOW);
     }
 
-    if (ldrValue3 < THRESHOLD) {
+    if (mensagem[2] == 0) {
       digitalWrite(LED_PIN3, HIGH);
     } else {
       digitalWrite(LED_PIN3, LOW);
     }
-//para o ldr 4, o valor pode ser diferente por causa do modelo mais sensível do LDR
-    if (ldrValue4 < THRESHOLD) {
+
+    if (mensagem[3] == 0) {
       digitalWrite(LED_PIN4, HIGH);
     } else {
       digitalWrite(LED_PIN4, LOW);
     }
 
     // Verifica se a temperatura está acima de 50 graus Celsius
-    if (temperatura > 50.0) { 
+    /*if (temperatura > 50.0) { 
       digitalWrite(LED_TEMP, HIGH); // Liga o LED de temperatura
     } else {
       digitalWrite(LED_TEMP, LOW); // Desliga o LED de temperatura
-    }
+    }*/
+
+    // Imprime o vetor no console serial para depuração
+    imprimirMensagem(mensagem);
   }
 }
